62dff5ed62bba22a9b1aa946f2e996ec18465fe5
xiaou1501 <huwei1501@126.com>
2023-04-25 07:56:45 +0800

    base code

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	_ "loveHome/routers"
+	"net/http"
+	"strings"
+
+	"github.com/astaxie/beego/context"
+
+	"github.com/astaxie/beego"
+)
+
+func ignoreStaticPath() {
+	//过滤器
+	beego.InsertFilter("/", beego.BeforeRouter, TransparentStatic)
+	beego.InsertFilter("/*", beego.BeforeRouter, TransparentStatic)
+}
+
+func TransparentStatic(ctx *context.Context) {
+	orpath := ctx.Request.URL.Path
+	log.Println(orpath)
+	//如果url中有api，则取消静态路由重定向
+	if strings.Index(orpath, "api") >= 0 {
+		return
+	}
+	http.ServeFile(ctx.ResponseWriter, ctx.Request, "static/html/"+orpath)
+}
+
+func main() {
+	ignoreStaticPath()
+	beego.Run()
+}
